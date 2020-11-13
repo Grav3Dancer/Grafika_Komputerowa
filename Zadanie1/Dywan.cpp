70436420a3c932c440c305c6ae45d1d0ecd069d7
Grav3Dancer <49979177+Grav3Dancer@users.noreply.github.com>
2020-11-13 10:07:58 +0100

    Rename Dywan.cpp to Zadanie1/Dywan.cpp

--- Zadanie1/Dywan.cpp
+++ Zadanie1/Dywan.cpp
@@ -7,40 +7,40 @@
 
 using namespace std;
 
-int poziomy; //Liczba poziomów dywanu 
+int poziomy; //Liczba poziomÃ³w dywanu 
 
-float rnd_kolor() // funkcja generuj¹ca losowy kolor  
+float rnd_kolor() // funkcja generujÄ…ca losowy kolor  
 {
 	return static_cast <float> (rand()) / static_cast <float> (RAND_MAX); 
 }
 
-float rnd_nierownosc(float mnoznik) // funkcja generuj¹ca losow¹ nierównoœæ ("poturbowanie" dywanu)
+float rnd_nierownosc(float mnoznik) // funkcja generujÄ…ca losowÄ… nierÃ³wnoÅ›Ä‡ ("poturbowanie" dywanu)
 {
 	return mnoznik * 0.02 * (static_cast <float> (rand()) / static_cast <float> (RAND_MAX));
 }
 
-void rysujKwadrat(float x, float y, float rozmiar) //  funkcja rysuj¹ca pojedynczy kwadrat
+void rysujKwadrat(float x, float y, float rozmiar) //  funkcja rysujÄ…ca pojedynczy kwadrat
 {
 	glRectf(x, y, x + rozmiar, y - rozmiar);
-	glColor3f(rnd_kolor(), rnd_kolor(), rnd_kolor()); // ustalanie koloru z u¿yciem funkcji losuj¹cej		
+	glColor3f(rnd_kolor(), rnd_kolor(), rnd_kolor()); // ustalanie koloru z uÅ¼yciem funkcji losujÄ…cej		
 }
 
-void rysujDywan(float x, float y, float rozmiar, float obecnyPoziom) // "g³ówna" funkcja, tworz¹ca Dywan Sierpiñskiego
+void rysujDywan(float x, float y, float rozmiar, float obecnyPoziom) // "gÅ‚Ã³wna" funkcja, tworzÄ…ca Dywan SierpiÅ„skiego
 {
 	for (int i = 0; i < 3; i++)
 	{
 		for (int j = 0; j < 3; j++)
 		{
-			if ((i == 1) && (j == 1)) // sprawdzenie czy œrodek rozwa¿¹nego kwadratu
+			if ((i == 1) && (j == 1)) // sprawdzenie czy Å›rodek rozwaÅ¼Ä…nego kwadratu
 			{ }
 			else
 			{
 				if (obecnyPoziom > poziomy - 2) //sprawdzenie czy funkcja jest na wymaganym poziomie :
-				{	// jeœli tak, to tysowane s¹ kwadraty
+				{	// jeÅ›li tak, to tysowane sÄ… kwadraty
 					rysujKwadrat(x + i*(rozmiar/3) + rnd_nierownosc(rozmiar), y - j*(rozmiar/3) + rnd_nierownosc(rozmiar), rozmiar/3);
 				}
 				else
-				{	//jeœli nie, ponowne wywo³anie funkcji rysuj¹cej dywan - zag³êbienie siê o jeden poziom dalej
+				{	//jeÅ›li nie, ponowne wywoÅ‚anie funkcji rysujÄ…cej dywan - zagÅ‚Ä™bienie siÄ™ o jeden poziom dalej
 					rysujDywan(x + i*(rozmiar/3) + rnd_nierownosc(rozmiar), y - j*(rozmiar/3) + rnd_nierownosc(rozmiar), rozmiar/3, obecnyPoziom+1);
 				}
 			}
@@ -53,34 +53,34 @@ void rysujDywan(float x, float y, float rozmiar, float obecnyPoziom) // "g³ówna"
 void RenderScene()
 {
 	glClear(GL_COLOR_BUFFER_BIT); 
-	// Czyszczenie okna aktualnym kolorem czyszcz¹cym
+	// Czyszczenie okna aktualnym kolorem czyszczÄ…cym
 
-	rysujDywan(-50.0f, 50.0f, 100.0f, 0); // Wywo³anie stworzonej funkcji, rysuj¹cej Dywan Sierpiñskiego
+	rysujDywan(-50.0f, 50.0f, 100.0f, 0); // WywoÅ‚anie stworzonej funkcji, rysujÄ…cej Dywan SierpiÅ„skiego
 
 	glFlush(); 
-	// Przekazanie poleceñ rysuj¹cych do wykonania
+	// Przekazanie poleceÅ„ rysujÄ…cych do wykonania
 }
 
 void ChangeSize(GLsizei horizontal, GLsizei vertical)
 {
 	GLfloat AspectRatio; 
-	// Deklaracja zmiennej AspectRatio okreœlaj¹cej proporcjê wymiarów okna
+	// Deklaracja zmiennej AspectRatio okreÅ›lajÄ…cej proporcjÄ™ wymiarÃ³w okna
 
 	if (vertical == 0) vertical = 1;
 	// Zabezpieczenie pzred dzieleniem przez 0
 
 	glViewport(0, 0, horizontal, vertical); 
-	// Ustawienie wielkoœciokna okna urz¹dzenia (Viewport)
+	// Ustawienie wielkoÅ›ciokna okna urzÄ…dzenia (Viewport)
 	// W tym przypadku od (0,0) do (horizontal, vertical)
 
 	glMatrixMode(GL_PROJECTION); 
-	// Okreœlenie uk³adu wspó³rzêdnych obserwatora
+	// OkreÅ›lenie ukÅ‚adu wspÃ³Å‚rzÄ™dnych obserwatora
 
 	glLoadIdentity();  
-	// Okreœlenie przestrzeni ograniczaj¹cej
+	// OkreÅ›lenie przestrzeni ograniczajÄ…cej
 
 	AspectRatio = (GLfloat)horizontal / (GLfloat)vertical; 
-	// Wyznaczenie wspó³czynnika proporcji okna
+	// Wyznaczenie wspÃ³Å‚czynnika proporcji okna
 
 	if (horizontal <= vertical)
 		glOrtho(-100.0, 100.0, -100.0 / AspectRatio, 100.0 / AspectRatio, 1.0, -1.0);
@@ -89,7 +89,7 @@ void ChangeSize(GLsizei horizontal, GLsizei vertical)
 		glOrtho(-100.0 * AspectRatio, 100.0 * AspectRatio, -100.0, 100.0, 1.0, -1.0);
 
 	glMatrixMode(GL_MODELVIEW); 
-	// Okreœlenie uk³adu wspó³rzêdnych
+	// OkreÅ›lenie ukÅ‚adu wspÃ³Å‚rzÄ™dnych
 
 	glLoadIdentity();
 }
@@ -99,22 +99,22 @@ int main(int argc, char** argv)
 	cout<<"Podaj liczbe poziomow dywanu: ";
 
 	cin>>poziomy; 
-	// Wczytanie rz¹danej liczby poziomów
+	// Wczytanie rzÄ…danej liczby poziomÃ³w
 
 	srand(time(NULL));
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);   
-	// Ustawienie trybu wyœwietlania
-	// GLUT_SINGLE - pojedynczy bufor wyœwietlania
-    // GLUT_RGBA - model kolorów RGB
+	// Ustawienie trybu wyÅ›wietlania
+	// GLUT_SINGLE - pojedynczy bufor wyÅ›wietlania
+    // GLUT_RGBA - model kolorÃ³w RGB
 
-	glutCreateWindow("Grafika komputerowa - Dywan Sierpiñskiego");
-	// Utworzenie okna i okreœlenie treœci napisu w nag³ówku okna
+	glutCreateWindow("Grafika komputerowa - Dywan SierpiÅ„skiego");
+	// Utworzenie okna i okreÅ›lenie treÅ›ci napisu w nagÅ‚Ã³wku okna
 
 	glutDisplayFunc(RenderScene); 
-	// Okreœlenie, ¿e funkcja RenderScene bêdzie funkcj¹ zwrotn¹ (callback)
+	// OkreÅ›lenie, Å¼e funkcja RenderScene bÄ™dzie funkcjÄ… zwrotnÄ… (callback)
 
 	glutReshapeFunc(ChangeSize);
-	// Dla aktualnego okna ustala funkcjê zwrotn¹ odpowiedzialn¹ za zmiany rozmiaru okna
+	// Dla aktualnego okna ustala funkcjÄ™ zwrotnÄ… odpowiedzialnÄ… za zmiany rozmiaru okna
 
 	glutMainLoop();
 	// Funkcja uruchamia szkielet biblioteki GLUT
